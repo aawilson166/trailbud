ddd5b130400e10e6586fc48dba34556595a76120
Aaron Wilson <aaronwilson@Tests-MacBook-Pro.local>
2021-02-11 15:36:16 -0800

    working on create and update routes

--- public/app.js
+++ public/app.js
@@ -1,123 +1,178 @@
 class App extends React.Component {
     state = {
-        trails:[]
+        name: '',
+        location: '',
+        // description: '',
+        // length: '',
+        // image: '',
+        trails:[],
     }
 
-    componentDidMount = () => {
-        axios.get('/api/trails').then(
-            (response) => {
-                this.setState({
-                    trails:response.data
-                })
-            }
-        )
-    }
+    // createTrail = (event) => {
+    //     event.preventDefault();
+    //     axios.post(
+    //         '/api/trails',
+    //         {
+    //             name:this.state.newTrailName,
+    //             location:this.state.newTrailLocation,
+    //         }
+    //     ).then(
+    //         (response) => {
+    //             this.setState({
+    //                 trails:response.data
+    //             })
+    //         }
+    //     )
+    // }
 
-    createTrail = (event) => {
-        event.preventDefault();
-        axios.post(
-            '/api/trails',
-            {
-                name:this.state.newTrailName,
-                location:this.state.newTrailLocation,
-            }
-        ).then(
-            (response) => {
-                this.setState({
-                    trails:response.data
-                })
-            }
-        )
-    }
+    // changeNewTrailLocation = (event) => {
+    //     this.setState({
+    //         newTrailLocation:event.target.value
+    //     });
+    // }
 
-    changeNewTrailLocation = (event) => {
+    // changeNewTrailName = (event) => {
+    //     this.setState({
+    //         newTrailName:event.target.value
+    //     });
+    // }
+
+    // deleteTrail = (event) => {
+    //     axios.delete('/api/trails/' + event.target.value).then(
+    //         (response) => {
+    //             this.setState({
+    //                 trails:response.data
+    //             })
+    //         }
+    //     )
+
+    // }
+
+    // updateTrail = (event) => {
+    //     event.preventDefault();
+    //     const id = event.target.getAttribute('id');
+    //     axios.put(
+    //         '/api/trails/' + id,
+    //         {
+    //             name:this.state.updateTrailName,
+    //             location:this.state.updateTrailLocation,
+    //         }
+    //     ).then(
+    //         (response) => {
+    //             this.setState({
+    //                 trails:response.data,
+    //                 name:'',
+    //                 location:'',
+    //             })
+    //         }
+    //     )
+    // }
+
+    // changeUpdateTrailName = (event) => {
+    //     this.setState(
+    //         {
+    //             updateTrailName:event.target.value
+    //         }
+    //     )
+    // }
+
+    // changeUpdateTrailLocation = (event) => {
+    //     this.setState(
+    //         {
+    //             updateTrailLocation:event.target.value
+    //         }
+    //     )
+    // }
+
+        // componentDidMount = () => {
+    //     axios.get('/api/trails').then(
+    //         (response) => {
+    //             this.setState({
+    //                 trails:response.data
+    //             })
+    //         }
+    //     )
+    // }
+
+
+    //==========================================================
+    //new functions
+    //==========================================================
+    handleChange = (event) => {
         this.setState({
-            newTrailLocation:event.target.value
-        });
+            [event.target.id]: event.target.value
+        })
     }
 
-    changeNewTrailName = (event) => {
-        this.setState({
-            newTrailName:event.target.value
-        });
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('/api/trails/', this.state).then((response) => {
+            this.getTrails()
+        })
     }
 
     deleteTrail = (event) => {
-        axios.delete('/api/trails/' + event.target.value).then(
-            (response) => {
-                this.setState({
-                    trails:response.data
-                })
-            }
-        )
-
+        axios.delete('/api/trails/' + event.target.value).then(response => {
+            this.getTrails()
+        })
     }
 
     updateTrail = (event) => {
-        event.preventDefault();
-        const id = event.target.getAttribute('id');
-        axios.put(
-            '/api/trails/' + id,
-            {
-                name:this.state.updateTrailName,
-                location:this.state.updateTrailLocation,
-            }
-        ).then(
-            (response) => {
-                this.setState({
-                    trails:response.data,
-                    name:'',
-                    location:'',
-                })
-            }
-        )
+        event.preventDefault()
+        //event.target.reset()
+        const id = event.target.id
+        axios.put('/api/trails/' + id, this.state).then(response => {
+            this.getTrails()
+        })
     }
 
-    changeUpdateTrailName = (event) => {
-        this.setState(
-            {
-                updateTrailName:event.target.value
-            }
+    getTrails = () => {
+        axios
+            .get('/api/trails/')
+            .then(
+            (response) =>
+                this.setState({trails: response.data, name: '', location: ''}),
+            (err) => console.log(err)            
         )
+        // .catch((error) => console.error(error))
+    }
+ 
+    componentDidMount = () => {
+        this.getTrails()
     }
 
-    changeUpdateTrailLocation = (event) => {
-        this.setState(
-            {
-                updateTrailLocation:event.target.value
-            }
-        )
-    }
+    //==========================================================
+    //==========================================================
 
     render = () => {
         return <div>
             <h2>Create Trail</h2>
-            <form onSubmit={this.createTrail}>
-                <input onKeyUp={this.changeNewTrailName} type="text" placeholder="name" /><br/>
-                <input onKeyUp={this.changeNewTrailLocation} type="text" placeholder="location" /><br/>
+            <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} type="text" placeholder="name" /><br/>
+                <input onChange={this.handleChange} type="text" placeholder="location" /><br/>
                 <input type="submit" value="Create Trail" />
             </form>
             <h2>List of trails</h2>
-            <ul>
+            <div>
                 {
                     this.state.trails.map(
-                        (trail, index) => {
-                            return <li key={index}>
+                        (trail) => {
+                            return <div key={trail.id}>
 
                                 {trail.name}: {trail.location}
 
                                 <button value={trail.id} onClick={this.deleteTrail}>DELETE</button>
 
                                 <form id={trail.id} onSubmit={this.updateTrail}>
-                                    <input onKeyUp={this.changeUpdateTrailName} type="text" placeholder="name"/><br/>
-                                    <input onKeyUp={this.changeUpdateTrailLocation} type="number" placeholder="location"/><br/>
+                                    <input onChange={this.handleChange} type="text" placeholder="name"/><br/>
+                                    <input onChange={this.handleChange} type="text" placeholder="location"/><br/>
                                     <input type="submit" value="Update Trail"/>
                                 </form>
-                            </li>
+                            </div>
                         }
                     )
                 }
-            </ul>
+            </div>
         </div>
     }
 
