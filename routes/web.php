fe7f333ca2570eca9e39f20c2b818d3583ab20fd
Aaron Wilson <aaronwilson@Tests-MacBook-Pro.local>
2021-02-15 09:49:58 -0800

    trying to remove index from url

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index.html', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('index.html');
 });
